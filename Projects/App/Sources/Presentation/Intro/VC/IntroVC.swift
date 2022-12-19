aa9aee67a06f16fe9452a7bdb17fee5c3e2f08a5
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-12-19 15:08:09 +0900

    :sparkles: intro publishing

--- Projects/App/Sources/Presentation/Intro/VC/IntroVC.swift
+++ Projects/App/Sources/Presentation/Intro/VC/IntroVC.swift
@@ -9,8 +9,31 @@ final class IntroVC: BaseVC<IntroVM>{
     
     private let logoImageView = UIImageView(image: UIImage(named: "Slam_Logo"))
     
+    private let mainLabel = UILabel().then{
+        $0.text = "매일 SLAM만의 챌린지를 친구들과 해봐!"
+        $0.font = UIFont(name: "Helvetica-Bold", size: 13)
+        $0.textColor = UIColor.white
+        $0.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 18.5)
+    }
+    
     private let signInButton = UIButton().then{
         $0.backgroundColor = SlamAsset.Colors.slamMainColor.color
+        $0.layer.cornerRadius = 18
+        $0.setTitle("로그인", for: .normal)
+        $0.setTitleColor(UIColor.black, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Helvetica-Bold", size: 20)
+    }
+    
+    private let SignUpLabel = UILabel().then{
+        $0.text = "SLAM이 처음이신가요?"
+        $0.font = UIFont(name: "Helvetica", size: 13)
+        $0.textColor = UIColor.white
+    }
+    
+    private let signUpButton = UIButton().then{
+        $0.setTitle("회원가입", for: .normal)
+        $0.setTitleColor(SlamAsset.Colors.slamMainColor.color, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Helvetica", size: 13)
     }
     
     override func viewDidLayoutSubviews() {
@@ -24,7 +47,7 @@ final class IntroVC: BaseVC<IntroVM>{
     
     
     override func addView() {
-        [logoImageView,signInButton].forEach {
+        [logoImageView,signInButton,SignUpLabel,signUpButton,mainLabel].forEach {
             view.addSubview($0)
         }
     }
@@ -36,6 +59,29 @@ final class IntroVC: BaseVC<IntroVM>{
             make.width.equalTo(300)
             make.height.equalTo(156)
         }
-        
+        signInButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(74)
+            make.width.equalToSuperview().inset(20)
+            make.height.equalTo(50)
+        }
+        SignUpLabel.snp.makeConstraints { make in
+            make.left.equalTo(109)
+            make.top.equalTo(signInButton.snp.bottom).offset(16)
+            make.width.equalTo(130)
+            make.height.equalTo(15)
+        }
+        signUpButton.snp.makeConstraints { make in
+            make.centerY.equalTo(SignUpLabel)
+            make.left.equalTo(SignUpLabel.snp.right).offset(5)
+            make.width.equalTo(50)
+            make.height.equalTo(15)
+        }
+        mainLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(logoImageView.snp.bottom).inset(10)
+            make.width.equalTo(242)
+            make.height.equalTo(15)
+        }
     }
 }
